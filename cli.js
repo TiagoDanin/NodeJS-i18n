66bb2465317ae12be1de35e27261172f6235dedb
TiagoDanin <TiagoDanin@outlook.com>
2019-04-06 13:17:31 -0300

    fix: parser strings

--- cli.js
+++ cli.js
@@ -21,11 +21,12 @@ const prefixs = [
 	'i18n',
 	'_'
 ]
-let comments = []
+const comments = []
+
 let n = 0
 let check = false
 
-let po = {
+const po = {
 	charset: 'utf-8',
 	headers: {
 		'content-type': 'text/plain; charset=UTF-8',
@@ -36,8 +37,8 @@ let po = {
 
 rl.on('line', (line) => {
 	line = line.replace(/^\.\//g, '')
-	let file = path.resolve(process.cwd() + '/' +  line)
-	let code = fs.readFileSync(file).toString()
+	const file = path.resolve(process.cwd() + '/' +  line)
+	const code = fs.readFileSync(file).toString()
 
 	let tokens = esprima.tokenize(code, {
 		loc: true,
@@ -45,18 +46,29 @@ rl.on('line', (line) => {
 	})
 
 	tokens = tokens.reduce((total, current, index) => {
-		if (argv.all) {
+		if (argv.all && !check) {
 			check = true
 		}
 		if (current.type == 'LineComment' && current.value.match(/i18n/i)) {
 			comments.push(current)
 		} else if (current.type == 'Identifier' && prefixs.includes(current.value)) {
 			check = true
-		} else if (check && whiteList.includes(current.type)) {
-			if (current.type == whiteList[0]) {
+		} else if (check) {
+			if (whiteList.includes(current.type) && typeof check == 'boolean') {
+				check = current.type
+			}
+			if (!whiteList.includes(current.type)) {
+				if (current.type == 'Punctuator' && current.value == ')') {
+					check = false
+				}
+				return total
+			}
+
+			if (check == whiteList[0]) { //String
 				total.push(current)
+				//check = false
 			} else if (current.value.startsWith('}')) {
-				let old = total[total.length-1]
+				const old = total[total.length-1]
 				total[total.length-1].value = `${old.value}${n}${current.value}`
 				total[total.length-1].loc.end = current.loc.end
 				n++
@@ -64,15 +76,15 @@ rl.on('line', (line) => {
 				n = 0
 				total.push(current)
 			}
-			if (
+			if (check == current.type && (
 				total[total.length-1].value.endsWith('"') ||
 				total[total.length-1].value.endsWith('\'') ||
 				total[total.length-1].value.endsWith('`')
-			) {
-				let comment = comments[comments.length-1]
+			)) {
+				const comment = comments[comments.length-1]
 				if (comment) {
-					let commentLine = comment.loc.start.line
-					let codeLine = total[total.length-1].loc.start.line
+					const commentLine = comment.loc.start.line
+					const codeLine = total[total.length-1].loc.start.line
 					if (commentLine == codeLine || commentLine+1 == codeLine) {
 						total[total.length-1].comment = comment.value.replace(/[#\s]*i18n[\s:]*/i, '')
 					}
@@ -84,21 +96,32 @@ rl.on('line', (line) => {
 	}, [{value: ''}])
 
 	tokens = tokens.filter(t => t.value != '')
-	tokens.forEach((token) => {
-		token.value = token.value.replace(/['"`](.*)['"`]/, '$1')
-		po.translations[''][token.value] = {
-			msgid: token.value,
-			comments: {
-				reference: `${line}:${token.loc.start.line}:${token.loc.start.column}`
-			},
-			msgstr: []
-		}
-		if (token.comment) {
-			po.translations[''][token.value].comments.extracted = token.comment
+	tokens.map((token) => {
+		token.value = token.value.replace(/['"`](.*)['"`]/s, '$1')
+		if (Object.keys(po.translations['']).includes(token.value)) {
+			po.translations[''][token.value].comments.reference += (
+				`\n${line}:${token.loc.start.line}:${token.loc.start.column}`
+			)
+			if (token.comment) {
+				po.translations[''][token.value].comments.extracted += '\n' + token.comment
+			}
+		} else {
+			po.translations[''][token.value] = {
+				msgid: token.value,
+				comments: {
+					reference: `${line}:${token.loc.start.line}:${token.loc.start.column}`
+				},
+				msgstr: []
+			}
+			if (token.comment) {
+				po.translations[''][token.value].comments.extracted = token.comment
+			}
 		}
 	})
 })
 
 rl.on('close', async (line) => {
-	console.log(gettextParser.po.compile(po).toString())
+	console.log(
+		gettextParser.po.compile(po).toString().trim().replace(/\\\\/sg, '\\')
+	)
 })
